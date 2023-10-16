51e3bade86df4f16c96741d6ad45fb580eee2640
jdashbaljir <j.dashbaljir@gmail.com>
2023-10-16 10:25:42 +0800

    wqerwqerweqr

--- vision_rpi_bot/vision_rpi_bot/publisher.py
+++ vision_rpi_bot/vision_rpi_bot/publisher.py
@@ -9,7 +9,7 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__('simple_publisher')
         self.publisher_ = self.create_publisher(Int16, '/pub_topic', 10)
-        timer_period = 0.2  # seconds
+        timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.iterator = 0
 
